c13402a5883898cdcf1f35f73f71ddc9f06b888f
rouny1ak <72198740+rouny1ak@users.noreply.github.com>
2021-09-14 23:09:47 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const b1 = document.querySelector('.b1');
-const mymenu = document.querySelector('.menu');
-
-b1.addEventListener('click', function (){
-    this.classList.toggle('is-active');
-    mymenu.classList.toggle('is-active');
-})
-
